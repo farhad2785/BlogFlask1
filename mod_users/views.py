f04ccb607abd68e1a892170e2b634fd638c3f372
d.jafari <davood.jafari@gmail.com>
2021-08-18 11:50:24 +0430

    Lesson 23 : Handling Errors

--- mod_users/views.py
+++ mod_users/views.py
@@ -1,4 +1,5 @@
 from flask import request, render_template, flash
+from sqlalchemy.exc import IntegrityError
 from app import db
 from . import users
 from .forms import Register_Form
@@ -14,11 +15,22 @@ def register():
         if not form.password.data == form.confirm_password.data:
             error_msg = 'Password and Confirm Password does not match!'
             form.confirm_password.errors.append(error_msg)
+        # اگر فیلدهای بیشتری برای یونیک بودن وجود داشته باشد نمی شود که از ترای استفاده کرد
+        # و باید به شکل زیر اول یه کوری زد و سرچ کرد
+        # find_user = User.query.filter(User.email.ilike(form.email.data)).first()
+        # if find_user:
+        #     flash('Email is in use.','Error')
+        #     return render_template('users/register.html', form=form)
         new_user = User()
         new_user.full_name = form.full_name.data
         new_user.email = form.email.data
         new_user.set_password(form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Your account was create successfully.','success')
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Your account was create successfully.','success')
+        # except Exception
+        except IntegrityError:
+            db.session.rollback()
+            flash('Email is in use.','Error')
     return render_template('users/register.html', form=form)
